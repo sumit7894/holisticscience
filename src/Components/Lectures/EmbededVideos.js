efae5d66b379512d767b8dc1b9a5aeacc929902c
Sumit Maurya <smithdashdash@gmail.com>
2023-12-31 16:39:19 +0530

    added youtube lectures

--- src/Components/Lectures/EmbededVideos.js
+++ src/Components/Lectures/EmbededVideos.js
@@ -1,17 +1,55 @@
-import React from 'react'
+import React from 'react';
+import './embededvideos.css'
 
 const EmbededVideos = () => {
   return (
-    <div>
-        <iframe width="560" height="315" 
-        src="https://www.youtube.com/embed/r0_NMYYsaCs?si=Al9Qx3ChPFgsVdye?&autoplay=1&mute=1" 
-        title="YouTube video player" 
-        frameborder="0" 
-        allow="accelerometer; 
-        autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen>
-        </iframe>
+    <div className="lecture-container">
+      <h1>Trending YouTube Lectures</h1>
+      <div className="lecture-grid">
+        {/* YouTube Embeds */}
+        <div className="lecture-item">
+          <iframe
+            width="100%"
+            height="315"
+            src="https://www.youtube.com/embed/_uQbCuwk0Io?si=weQATtxFlDG2mvsr"
+            title="Lecture 1"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe>
+        </div>
+        <div className="lecture-item">
+          <iframe
+            width="100%"
+            height="315"
+            src="https://www.youtube.com/embed/Q2X5pbnwga4?si=oWZZ4yaN_XU57jaT"
+            title="Lecture 2"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe>
+        </div>
+        <div className="lecture-item">
+          <iframe
+            width="100%"
+            height="315"
+            src="https://www.youtube.com/embed/man7iADwNnk?si=pAz0dMWQkiOPidUA"
+            title="Lecture 3"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe>
+        </div>
+        <div className="lecture-item">
+          <iframe
+            width="100%"
+            height="315"
+            src="https://www.youtube.com/embed/x1Y08TidvXI?si=caZNyRN3zXT9Rl2M"
+            title="Lecture 4"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default EmbededVideos
+export default EmbededVideos;
--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -1,10 +1,46 @@
 import React from 'react'
 import NavBar from '../../Components/Navbar/Navbar'
-
+import './contact.css'
+import { SocialIcon } from 'react-social-icons'
 const Contact = () => {
   return (
     <div>
         <NavBar/>
+        <div className="contact-us-page">
+      <h2>Contact Us</h2>
+      <div className="social-links">
+        <div className="social-link" onClick={(e) =>  {
+          e.preventDefault();
+          window.open('https://t.me/holisticsciencesgs', '_blank')}}>
+          <SocialIcon url="https://telegram.com" style={{ height: 100, width: 100 }}/>
+        </div>
+        <div className="social-link" onClick={(e) => 
+          {
+          e.preventDefault();
+          window.open('https://wa.me/8824812179', '_blank')}}>
+          <SocialIcon url="https://whatsapp.com" style={{ height: 100, width: 100 }}/>
+        </div>
+        <div className="social-link" onClick={(e) => 
+        {
+        e.preventDefault();
+        window.open('https://www.instagram.com/holistic_science_sgs', '_blank')}}>
+        <SocialIcon url="https://instagram.com" style={{ height: 100, width: 100 }}/>
+        </div>
+        <div className="social-link" onClick={(e) =>
+          {
+            e.preventDefault();
+          window.open('https://pin.it/5ap5592', '_blank')}}>
+       <SocialIcon network="pinterest" style={{ height: 100, width: 100 }} />
+        </div>
+
+        <div className="social-link" onClick={(e) => 
+          {
+            e.preventDefault();
+          window.open('https://youtube.com/@holisticsciencesgs', '_blank')}}>
+          <SocialIcon url="https://youtube.com" style={{ height: 100, width: 100 }}/>
+        </div>
+      </div>
+    </div>
     </div>
   )
 }
