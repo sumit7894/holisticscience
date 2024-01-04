964573c0f8d3b87508668bf317f2bf92cac3979e
Sumit Maurya <smithdashdash@gmail.com>
2024-01-04 22:47:39 +0530

    moved the hamburger menu from right to left

--- src/Components/Blog/BlogCards/BlogCards.js
+++ src/Components/Blog/BlogCards/BlogCards.js
@@ -1,10 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import {db} from '../../../firebase-config'
-import { collection,  doc,  getDoc,  getDocs } from 'firebase/firestore'
+import { collection,   getDocs } from 'firebase/firestore'
 import './blogcard.css'
 import { useNavigate } from 'react-router-dom';
 const BlogCards = () => {
-  const contentElement = document.getElementById("content-display");
   const navigate = useNavigate();
   const [blogPosts, setBlogPosts] = useState([]); 
   const [selectedPost, setSelectedPost] = useState(null); // State for the selected blog post
@@ -17,6 +16,7 @@ const BlogCards = () => {
   }
 
   useEffect(()=>{
+    // eslint-disable-next-line
     getData();
   },[]);
 
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,7 @@
-import React, { useEffect, useState } from 'react'
 import './home.css'
 import NavBar from '../../Components/Navbar/Navbar'
 import Hero from '../../Components/HeroSection/Hero'
 import Testimonial from '../../Components/Testimonial/Testimonial'
-
-// import NavBar from '../../Components/Navbar/NavBar'
 const Home = () => {
   
   return (
